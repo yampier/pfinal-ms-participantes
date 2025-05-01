31e885cab690c5adeaf6f337d5c4132c08183979
Jean <jgarciaal@192.168.1.87>
2025-04-30 21:29:27 -0500

    init

---
+++ src/main/java/pe/gob/sunat/participantes/MsParticipantesApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.sunat.participantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParticipantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsParticipantesApplication.class, args);
+	}
+
+}

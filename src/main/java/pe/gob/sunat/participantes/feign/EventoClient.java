4940464d52c49a831e666517442ca495a44f6af0
jean <yampier.g@gmail.com>
2025-05-15 17:51:59 -0600

    arhchivos compose

--- src/main/java/pe/gob/sunat/participantes/feign/EventoClient.java
+++ src/main/java/pe/gob/sunat/participantes/feign/EventoClient.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import pe.gob.sunat.participantes.dto.EventoDTO;
 
 @FeignClient(name = "ms-eventos", url = "http://ms-eventos:9084")
-//@FeignClient(name = "ms-eventos", url = "http://localhost:9084")
 public interface EventoClient {
 
     @GetMapping("/eventos/{codigo}")
